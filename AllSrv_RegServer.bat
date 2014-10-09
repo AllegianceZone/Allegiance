79f8e6d21de2867878b72b2ba2d5a96e94d41e98
ImagoTrigger <imagotrigger@gmail.com>
2014-10-08 23:29:53 -0500

    update the handy batch scripts

--- AllSrv_RegServer.bat
+++ AllSrv_RegServer.bat
@@ -1,5 +1,19 @@
 regsvr32 /s /u AGC.dll
 regsvr32 /s AGC.dll
+regsvr32 /s /u TCObj.dll
+regsvr32 /s TCObj.dll
+regsvr32 /s /u PigsLib.dll
+regsvr32 /s PigsLib.dll
+Alllobby -UnRegServer
 Alllobby -RegServer
-AllSrvMP -RegServer
+rem AllSrvMP -UnRegServer
+rem AllSrvMP -RegServer
+AllSrv -UnRegServer
 AllSrv -RegServer
+AllClub -UnRegServer
+AllClub -RegServer
+PigAccts -UnRegServer
+PigAccts -RegServer %1 %2
+PigSrv -UnRegServer
+PigSrv -RegServer %1 %2
+
