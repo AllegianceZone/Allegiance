07dd9539a5cfca552d67292b2393d988341ece75
ImagoTrigger <imagotrigger@gmail.com>
2014-10-12 10:54:40 -0500

    more error handling for the new pigcommand test script

--- src/Pigs/Scripts/PigCommand.pl
+++ src/Pigs/Scripts/PigCommand.pl
@@ -8,20 +8,31 @@ my $pigname = $ARGV[0] || 'botpilot1';
 print "Creating Pigs session\n";
 my $sess  = Win32::OLE->new("Pigs.Session");
 if ($sess) {
-	my $pig = $session->Pigs();
+	my $pig = $sess->Pigs($pigname);
 	if ($pig) {
 		print "$pigname is currently ".$pig->PigStateName."\n";
 		
-		###########################
-		#Put your command(s) here!#
-		###########################
+		#############################
+		# Put your command(s) here! #
+		#############################
 		
 		#$pig->JoinTeam("Iron Coalition");
-		#$pig->Ship->Attack("BBT");
+		#$pig->Ship->Attack("botpilot2");
 		
+		#############################
+		
+		my $err = Win32::OLE::LastError();
+		if ($err == 0) {
+			print "Ok!\n";
+			exit 0;
+		} else {
+			print "Pig $pigname: $err!\n";
+			exit 1;
+		}
+
 	} else {
 		my $err = Win32::OLE::LastError();
-		print "Pig $pigname: $err!\n";
+		($err == 0) ? print "Pig $pigname does not exist!\n" : print "Pig $pigname: $err!\n";
 		exit 1;
 	}
 } else {
