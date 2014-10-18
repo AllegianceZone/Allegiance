db4d6e9d865b35cd51bc97f5060906168c265a98
BigBetaTester <BigBetaTester@users.noreply.github.com>
2014-10-17 23:49:42 -0400

    Update shipIGC.cpp
    Change bot firing distance decision to equal lifespan of bullets until ship(s) velocity can be factored in.

--- src/Igc/shipIGC.cpp
+++ src/Igc/shipIGC.cpp
@@ -2377,7 +2377,7 @@ void    CshipIGC::PlotShipMove(Time          timeStop)
                     }
 
                     SetStateBits(~selectedWeaponMaskIGC,
-                                 ((da <= c_fMaxOffAngle) && (t <= lifespan * 1.07f))
+                                 ((da <= c_fMaxOffAngle) && (t <= lifespan * 1.01f))
                                  ? (state | allWeaponsIGC)
                                  : state);
 
