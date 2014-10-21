2f2c85988a9d8a556eade6888cee3947ab79a62d
ImagoTrigger <imagotrigger@gmail.com>
2014-10-20 21:30:21 -0500

    now include unicode version of mfc dll

--- Sh_GetRetail.bat
+++ Sh_GetRetail.bat
@@ -25,7 +25,7 @@ copy %2objs10\AZRetail\AutoUpdate\AutoUpdate.pdb %1 /Y
 copy %2objs10\AZRetail\Reloader\Reloader.exe %1 /Y
 copy %2objs10\AZRetail\Reloader\Reloader.pdb %1 /Y
 copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.ATL\atl100.dll" %1 /Y
-copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.MFC\mfc100.dll" %1 /Y
+copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.MFC\mfc100u.dll" %1 /Y
 copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.CRT\msvcp100.dll" %1 /Y
 copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.CRT\msvcr100.dll" %1 /Y
 copy %2src\license.txt %1 /Y
